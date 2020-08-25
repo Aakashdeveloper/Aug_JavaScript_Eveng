37c07749766379056b6da529a49b3b058579b521
aakashdeveloper <ahanda205@gmail.com>
2020-08-25 20:36:48 +0530

    A

---
+++ jquery/first.js
@@ -0,0 +1,9 @@
+$('h1')[0]
+<h1>​I'm AAKASH HANDA​</h1>​
+$('h1')[0].innerText
+"I'm AAKASH HANDA"
+$('h1')[0].innerText="I am from developer funnel"
+"I am from developer funnel"
+
+$('.hhhh')
+$('#jjnjf')
